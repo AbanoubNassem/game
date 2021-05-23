b10646446923e039bdf8c8ec06e3bab39630e05f
Theraot <Theraot@gmail.com>
2021-05-23 03:27:32 -0500

    Added exported variable hero_path to Map.gd

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -1,8 +1,7 @@
 extends StaticBody
 
-onready var hero = get_node('/root/World/Hero')
-
-
+export var hero_path:NodePath
+onready var hero = get_node(hero_path)
 
 func _on_Map_input_event(camera, event, click_position, click_normal, shape_idx):
 		if event is InputEventMouseButton and event.pressed:

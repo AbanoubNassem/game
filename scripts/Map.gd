8e12eb8b6bcc451276514f3f6ec9efd1dd7fc8e3
Theraot <Theraot@gmail.com>
2021-05-23 03:30:37 -0500

    Added a target indicator

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -6,6 +6,6 @@ onready var hero = get_node(hero_path)
 func _on_Map_input_event(camera, event, click_position, click_normal, shape_idx):
 		if event is InputEventMouseButton and event.pressed:
 			var t:Vector3 = click_position
-		
+			$TargetIndicator.global_transform.origin = t
 			hero.target = t
 			print(hero)
